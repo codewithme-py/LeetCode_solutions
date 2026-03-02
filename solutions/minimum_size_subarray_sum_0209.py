718ae73fce7755f7a55ace89d6b7e6e359cba5aa
Code With Me <codewithme@ya.ru>
2026-03-02 14:36:11 +0300

    problem solution & test 0008

--- solutions/minimum_size_subarray_sum_0209.py
+++ solutions/minimum_size_subarray_sum_0209.py
@@ -1,5 +1,11 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: list[int]) -> int:
+        """
+        Given an array of positive integers nums and a positive integer target,
+        return the minimal length of a contiguous subarray of which the sum is
+        greater than or equal to target.
+        If there is no such subarray, return 0.
+        """
         left_mark, current_sum, min_length = 0, 0, float('inf')
         for right_mark in range(len(nums)):
             current_sum += nums[right_mark]
